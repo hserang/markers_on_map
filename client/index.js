62f6a8c40bb5bf4550733a14ea61f4666efc9a5f
Haroun Serang <haroun@ripple.com>
2017-08-20 10:36:25 -0700

    Add header

--- client/index.js
+++ client/index.js
@@ -4,6 +4,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import WorldMap from './components/WorldMap'
 
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>Some Info Here</h1>
+      </header>
+    )
+  }
+}
+
+class AppContainer extends React.Component {
+  render() {
+    return (
+      <div className="appWrapper">
+        <Header/>
+        <WorldMap/>
+      </div>
+    )
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<WorldMap/>, document.getElementById('app'))
+  ReactDOM.render(<AppContainer/>, document.getElementById('app'))
 })
--- public/app.js
+++ public/app.js
@@ -15402,6 +15402,8 @@ equirectangularRaw.invert = equirectangularRaw;
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(49);
 
 var _react2 = _interopRequireDefault(_react);
@@ -15416,8 +15418,65 @@ var _WorldMap2 = _interopRequireDefault(_WorldMap);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Header = function (_React$Component) {
+  _inherits(Header, _React$Component);
+
+  function Header() {
+    _classCallCheck(this, Header);
+
+    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+  }
+
+  _createClass(Header, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'header',
+        { className: 'header' },
+        _react2.default.createElement(
+          'h1',
+          null,
+          'Some Info Here'
+        )
+      );
+    }
+  }]);
+
+  return Header;
+}(_react2.default.Component);
+
+var AppContainer = function (_React$Component2) {
+  _inherits(AppContainer, _React$Component2);
+
+  function AppContainer() {
+    _classCallCheck(this, AppContainer);
+
+    return _possibleConstructorReturn(this, (AppContainer.__proto__ || Object.getPrototypeOf(AppContainer)).apply(this, arguments));
+  }
+
+  _createClass(AppContainer, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { className: 'appWrapper' },
+        _react2.default.createElement(Header, null),
+        _react2.default.createElement(_WorldMap2.default, null)
+      );
+    }
+  }]);
+
+  return AppContainer;
+}(_react2.default.Component);
+
 document.addEventListener('DOMContentLoaded', function () {
-  _reactDom2.default.render(_react2.default.createElement(_WorldMap2.default, null), document.getElementById('app'));
+  _reactDom2.default.render(_react2.default.createElement(AppContainer, null), document.getElementById('app'));
 });
 
 /***/ }),
